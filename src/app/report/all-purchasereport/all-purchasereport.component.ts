d0af334f8bdcd4d1ab995c9da4b46e0d921519c7
alexjeff <46997932+alexjeff@users.noreply.github.com>
2020-01-24 16:03:58 +0530

    changes on 24

--- src/app/report/all-purchasereport/all-purchasereport.component.ts
+++ src/app/report/all-purchasereport/all-purchasereport.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-all-purchasereport',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllPurchasereportComponent implements OnInit {
 
-  constructor() { }
+  public purchaseList : any;
+  public purchaseorderhide=false;
+  model:any ={};
+
+  displayedColumns: string[] = ['No','PoInvoice','poDate','vendor','Total'];
+  dataSource: MatTableDataSource<any>;
+  
+  constructor() { 
+    const purchasedata = require("../../purchasereportdata.json");
+    this.purchaseList=purchasedata;
+    this.dataSource = new MatTableDataSource(this.purchaseList);
+  }
 
   ngOnInit() {
   }
-
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  purchaseorderdivcall(invoicenumber: string){
+    this.purchaseorderhide=true;
+    for(let i=0;i<this.purchaseList.length;i++){
+      if(this.purchaseList[i].invoiceNumber==invoicenumber){
+        this.model.invoiceNumber = this.purchaseList[i].invoiceNumber;
+        this.model.poDate = this.purchaseList[i].poDate;
+        this.model.vendorName1 = this.purchaseList[i].vendorName;
+        this.model.subTotal = this.purchaseList[i].subTotal;
+      }
+    }
+  }
 }
--- src/app/report/all-salesreport/all-salesreport.component.ts
+++ src/app/report/all-salesreport/all-salesreport.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-all-salesreport',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-salesreport.component.css']
 })
 export class AllSalesreportComponent implements OnInit {
+  public purchaseList : any;
+  public salesorderhide=false;
+  model:any ={};
 
-  constructor() { }
+  displayedColumns: string[] = ['No','SoInvoice','soDate','customer','Total'];
+  dataSource: MatTableDataSource<any>;
+  
+  constructor() { 
+    const purchasedata = require("../../salesreportdata.json");
+    this.purchaseList=purchasedata;
+    this.dataSource = new MatTableDataSource(this.purchaseList);
+  }
 
   ngOnInit() {
   }
-
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  salesorderdivcall(invoicenumber: string){
+    this.salesorderhide=true;
+    for(let i=0;i<this.purchaseList.length;i++){
+      if(this.purchaseList[i].invoiceNumber==invoicenumber){
+        this.model.invoiceNumber = this.purchaseList[i].invoiceNumber;
+        this.model.soDate = this.purchaseList[i].soDate;
+        this.model.customerName1 = this.purchaseList[i].customerName;
+        this.model.subTotal = this.purchaseList[i].subTotal;
+      }
+    }
+  }
 }
