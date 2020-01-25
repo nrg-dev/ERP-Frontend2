52174f4055b7430c7c0eafff76f8d1ef70a4ee64
alexjeff <46997932+alexjeff@users.noreply.github.com>
2020-01-25 13:13:06 +0530

    all report changes

--- src/app/report/all-financereport/all-financereport.component.ts
+++ src/app/report/all-financereport/all-financereport.component.ts
@@ -10,6 +10,7 @@ export class AllFinancereportComponent implements OnInit {
 
   model:any ={};
   public financeList : any;
+  public financedetailshide=false;
 
   displayedColumns: string[] = ['Date','Category','Description','Debit','Credit'];
   dataSource: MatTableDataSource<any>;
@@ -26,6 +27,21 @@ export class AllFinancereportComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
+    }
+  }
+
+  financedetaildivcall(Date: string){
+    this.financedetailshide=true;
+    for(let i=0;i<this.financeList.length;i++){
+      if(this.financeList[i].date==Date){
+        this.model.customer = this.financeList[i].customer;
+        this.model.date = this.financeList[i].date;
+        this.model.category = this.financeList[i].category;
+        this.model.description = this.financeList[i].description;
+        this.model.debit = this.financeList[i].debit;
+        this.model.credit = this.financeList[i].credit;
+        this.model.code = this.financeList[i].code;
+      }
     }
   }
 
--- src/app/report/all-stockreport/all-stockreport.component.ts
+++ src/app/report/all-stockreport/all-stockreport.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-all-stockreport',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllStockreportComponent implements OnInit {
 
-  constructor() { }
+  public purchaseList : any;
+  public salesorderhide=false;
+  model:any ={};
+
+  displayedColumns: string[] = ['ItemCode','ItemName','Category','Date','Status'];
+  dataSource: MatTableDataSource<any>;
+  constructor() {
+    const purchasedata = require("../../stockReportdata.json");
+    this.purchaseList=purchasedata;
+    this.dataSource = new MatTableDataSource(this.purchaseList);
+   }
 
   ngOnInit() {
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  stockdivcall(itemCode: string){
+    this.salesorderhide=true;
+    for(let i=0;i<this.purchaseList.length;i++){
+      if(this.purchaseList[i].itemCode==itemCode){
+        this.model.itemCode = this.purchaseList[i].itemCode;
+        this.model.productName = this.purchaseList[i].productName;
+        this.model.category = this.purchaseList[i].category;
+        this.model.lastUpdate = this.purchaseList[i].lastUpdate;
+        this.model.status = this.purchaseList[i].status;
+      }
+    }
+  }
 }
