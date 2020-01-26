6d64ead29b4ecfe82f76d49e6415843a96205a52
alexjeff <46997932+alexjeff@users.noreply.github.com>
2020-01-26 20:06:38 +0530

    changes in front end

--- src/app/vendorcustomer/vendoradd/vendoradd.component.ts
+++ src/app/vendorcustomer/vendoradd/vendoradd.component.ts
@@ -36,148 +36,8 @@ export class VendoraddComponent implements OnInit {
   customer:Customer;
   vendor:Vendor;
   vendorList: any = {};
+  customerList: any={};
 
-  /*vendorList : any = [ {
-    vendorcode:'VEN001',
-    vendorName :'PT NRG',
-    phoneNumber : '+62 878-2277-7490',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 2, Main Street,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'nrgadmin@neotural.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-  },
-  {vendorcode:'VEN002',
-  vendorName :'CV. PURNAMA RAYA FOOD',
-  phoneNumber : '+62 878-2277-7450',
-  mobileNumber : '+62 675-777-8998',
-  address : 'No 32, East Street,Jakarta,India',
-  country :  'Indonesia',
-  city : 'Jakarta',
-  email : 'purnama@raya.com',
-  lastedit : '17/9/2019',
-  addeddate : '1/9/2019'
-},
- {vendorcode:'VEN003',
-  vendorName :'AGARINDO BOGATAMA, PT',
-  phoneNumber : '+62 878-2233-7490',
-  mobileNumber : '+62 675-777-8998',
-  address : 'No 54, Kungan city,Jakarta,Indonesia',
-  country :  'Indonesia',
-  city : 'Jakarta',
-  email : 'admin@bogatama.com',
-  lastedit : '17/9/2019',
-  addeddate : '1/9/2019'
-},
-  {vendorcode:'VEN004',
-   vendorName :'Manggar Natural',
-    phoneNumber : '+62 878-2277-8490',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 23, Near T Rasuna,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'manggar@gmail.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-},
-{vendorcode:'VEN005',
-    vendorName :'CD Craft Bethesda - Denara',
-    phoneNumber : '+62 878-2277-7590',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 47, East Costal road ,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'admin@denara.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-},
-{vendorcode:'VEN006',
-    vendorName :'Golden Manyaran, PT',
-    phoneNumber : '+62 878-2277-7690',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 2/4, South Street,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'admin@golden.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-},
-
-];*/
-custList : any = [ 
-  {
-    custcode:'CUST001',
-    customerName :' WIROTO CRAFT',
-    phoneNumber : '+62 878-2277-7490',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 2, Main Street,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'nrgadmin@neotural.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-  },
-  {custcode:'CUST002',
-  customerName :'PT. Adi Jaya Naturindo',
-  phoneNumber : '+62 878-2277-7450',
-  mobileNumber : '+62 675-777-8998',
-  address : 'No 32, East Street,Jakarta,India',
-  country :  'Indonesia',
-  city : 'Jakarta',
-  email : 'purnama@raya.com',
-  lastedit : '17/9/2019',
-  addeddate : '1/9/2019'
-},
- {custcode:'CUST003',
-  customerName :'Editha Artcraft',
-  phoneNumber : '+62 878-2233-7490',
-  mobileNumber : '+62 675-777-8998',
-  address : 'No 54, Kungan city,Jakarta,Indonesia',
-  country :  'Indonesia',
-  city : 'Jakarta',
-  email : 'admin@bogatama.com',
-  lastedit : '17/9/2019',
-  addeddate : '1/9/2019'
-},
-  { custcode:'CUST004',
-    customerName :'PONDAN PANGAN MAKMUR INDONESIA PT',
-    phoneNumber : '+62 878-2277-8490',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 23, Near T Rasuna,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'manggar@gmail.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-},
-{   custcode:'CUST005',
-    customerName :'Sumatra Copper and Gold',
-    phoneNumber : '+62 878-2277-7590',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 47, East Costal road ,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'admin@denara.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-},
-{   
-  
-    custcode:'CUST006',
-    customerName :'MANOHARA ASRI PT',
-    phoneNumber : '+62 878-2277-7690',
-    mobileNumber : '+62 675-777-8998',
-    address : 'No 2/4, South Street,Jakarta,Indonesia',
-    country :  'Indonesia',
-    city : 'Jakarta',
-    email : 'admin@golden.com',
-    lastedit : '17/9/2019',
-    addeddate : '1/9/2019'
-},
-
-];
 
 countryList: any = ['India', 'Malaysia', 'Indonesia', 'Singapore'];
 
@@ -207,6 +67,7 @@ countryList: any = ['India', 'Malaysia', 'Indonesia', 'Singapore'];
     document.getElementById("customerstyle").style.borderBottom='none';
     document.getElementById("vendorstyle").style.fontWeight='bold';
     this.getAllVendorDetails();
+    this.getAllCustomerDetails();
   }
 
   getAllVendorDetails(){
@@ -217,8 +78,21 @@ countryList: any = ['India', 'Malaysia', 'Indonesia', 'Singapore'];
         this.vendorList = data;
       },
       error => {
+        alert("server error");
       }
     );
+  }
+  getAllCustomerDetails(){
+    this.customerList="";
+    this.customerService.load()
+    .subscribe(
+      data => {
+        this.customerList = data;
+      },
+      error => {
+        alert("server error");
+      }
+    )
   }
 
   //Vendor details methos start
@@ -264,85 +138,6 @@ countryList: any = ['India', 'Malaysia', 'Indonesia', 'Singapore'];
         break;
       }
     }
-/*
-    if(vendorcode == "VEN001"){
-      this.model.vendorcode = this.vendorList[0].vendorcode;
-      this.model.vendorName = this.vendorList[0].vendorName;
-      this.model.phoneNumber = this.vendorList[0].phoneNumber;
-      this.model.mobileNumber = this.vendorList[0].mobileNumber;
-      this.model.address = this.vendorList[0].address;
-      this.model.country = this.vendorList[0].country;
-      this.model.city = this.vendorList[0].city;
-      this.model.email = this.vendorList[0].email;
-      this.model.lastedit = this.vendorList[0].lastedit;
-      this.model.addeddate = this.vendorList[0].addeddate;
-      this.alertService.success("");
-
-    }else if(vendorcode == "VEN002"){
-      this.model.vendorcode = this.vendorList[1].vendorcode;
-      this.model.vendorName = this.vendorList[1].vendorName;
-      this.model.phoneNumber = this.vendorList[1].phoneNumber;
-      this.model.mobileNumber = this.vendorList[1].mobileNumber;
-      this.model.country = this.vendorList[1].country;
-      this.model.city = this.vendorList[1].city;
-      this.model.email = this.vendorList[1].email;
-      this.model.lastedit = this.vendorList[1].lastedit;
-      this.model.addeddate = this.vendorList[1].addeddate;
-      this.alertService.success("");
-    }
-    else if(vendorcode == "VEN003"){
-      this.model.vendorcode = this.vendorList[2].vendorcode;
-      this.model.vendorName = this.vendorList[2].vendorName;
-      this.model.phoneNumber = this.vendorList[2].phoneNumber;
-      this.model.mobileNumber = this.vendorList[2].mobileNumber;
-      this.model.address = this.vendorList[2].address;
-      this.model.country = this.vendorList[2].country;
-      this.model.city = this.vendorList[2].city;
-      this.model.email = this.vendorList[2].email;
-      this.model.lastedit = this.vendorList[2].lastedit;
-      this.model.addeddate = this.vendorList[2].addeddate;
-      this.alertService.success("");
-    }
-    else if(vendorcode == "VEN004"){
-      this.model.vendorcode = this.vendorList[3].vendorcode;
-      this.model.vendorName = this.vendorList[3].vendorName;
-      this.model.phoneNumber = this.vendorList[3].phoneNumber;
-      this.model.mobileNumber = this.vendorList[3].mobileNumber;
-      this.model.address = this.vendorList[3].address;
-      this.model.country = this.vendorList[3].country;
-      this.model.city = this.vendorList[3].city;
-      this.model.email = this.vendorList[3].email;
-      this.model.lastedit = this.vendorList[3].lastedit;
-      this.model.addeddate = this.vendorList[3].addeddate;
-      this.alertService.success("");
-    }
-    else if(vendorcode == "VEN005"){
-      this.model.vendorcode = this.vendorList[4].vendorcode;
-      this.model.vendorName = this.vendorList[4].vendorName;
-      this.model.phoneNumber = this.vendorList[4].phoneNumber;
-      this.model.mobileNumber = this.vendorList[4].mobileNumber;
-      this.model.address = this.vendorList[4].address;
-      this.model.country = this.vendorList[4].country;
-      this.model.city = this.vendorList[4].city;
-      this.model.email = this.vendorList[4].email;
-      this.model.lastedit = this.vendorList[4].lastedit;
-      this.model.addeddate = this.vendorList[4].addeddate;
-      this.alertService.success("");
-    }
-
-    else if(vendorcode == "VEN006"){
-      this.model.vendorcode = this.vendorList[5].vendorcode;
-      this.model.vendorName = this.vendorList[5].vendorName;
-      this.model.phoneNumber = this.vendorList[5].phoneNumber;
-      this.model.mobileNumber = this.vendorList[5].mobileNumber;
-      this.model.address = this.vendorList[5].address;
-      this.model.country = this.vendorList[5].country;
-      this.model.city = this.vendorList[5].city;
-      this.model.email = this.vendorList[5].email;
-      this.model.lastedit = this.vendorList[5].lastedit;
-      this.model.addeddate = this.vendorList[5].addeddate;
-      this.alertService.success("");
-    } */
   }
 
   vendoreditDetails(){
@@ -482,24 +277,24 @@ countryList: any = ['India', 'Malaysia', 'Indonesia', 'Singapore'];
     this.customerdragAndDrop = false;
 
     // this.custList; json list 
-    var lenth = this.custList.lenth;
-    console.log ("Cust Size -->" + this.custList.length);
+    var lenth = this.customerList.lenth;
+    console.log ("Cust Size -->" + this.customerList.length);
     console.log ("Cust code -->" + custcode);
 
-    for (let i = 0; i < this.custList.length; i++) {
+    for (let i = 0; i < this.customerList.length; i++) {
       console.log ("Block statement execution no." + i);
-      if(this.custList[i].custcode==custcode){
+      if(this.customerList[i].custcode==custcode){
         console.log("Inside if customer code -->"+custcode);
-        this.model.custcode = this.custList[i].custcode;
-        this.model.customerName = this.custList[i].customerName;
-        this.model.phoneNumber = this.custList[i].phoneNumber;
-        this.model.mobileNumber = this.custList[i].mobileNumber;
-        this.model.address = this.custList[i].address;
-        this.model.country = this.custList[i].country;
-        this.model.city = this.custList[i].city;
-        this.model.email = this.custList[i].email;
-        this.model.lastedit = this.custList[i].lastedit;
-        this.model.addeddate = this.custList[i].addeddate;
+        this.model.custcode = this.customerList[i].custcode;
+        this.model.customerName = this.customerList[i].customerName;
+        this.model.phoneNumber = this.customerList[i].phoneNumber;
+        this.model.mobileNumber = this.customerList[i].mobileNumber;
+        this.model.address = this.customerList[i].address;
+        this.model.country = this.customerList[i].country;
+        this.model.city = this.customerList[i].city;
+        this.model.email = this.customerList[i].email;
+        this.model.lastedit = this.customerList[i].lastedit;
+        this.model.addeddate = this.customerList[i].addeddate;
         break;
       }
 
