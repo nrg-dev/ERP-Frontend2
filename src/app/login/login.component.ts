64fe2a0afcc7d953f2c933c502629b107f595a27
alexjeff <46997932+alexjeff@users.noreply.github.com>
2020-03-02 15:28:48 +0530

    Changes on Mar 2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../_models/index';
 import { Router, ActivatedRoute } from '@angular/router';
-import { AlertService } from '../_services/index';
+import { AlertService,AuthenticationService } from '../_services/index';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -14,52 +14,138 @@ export class LoginComponent implements OnInit {
   user:User;
   loading = false;
   passwordtype="password";
+  errorStatus : any;
+  usererrorStatus: any;
+  passworderror: any;
 
-  constructor(  private router: Router, private alertService: AlertService) { }
+  public SignIndiv = false;
+  public usercheckdiv = false;
+  public passwordcheck = false; 
+
+  constructor(  
+    private router: Router, 
+    private alertService: AlertService,
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit() {
-   //document.getElementById('id01').style.display='block'";
-   //document.getElementById('id01').style.display='block';
-
-   
     this.model.currentusername='';
     this.model.currentpassword='';
+    this.SignIndiv = true;
+    this.usercheckdiv = false;
+    this.passwordcheck = false;
   }
 
   onClickSubmit(){
+    this.SignIndiv = true;
+    this.usercheckdiv = false;
+    this.passwordcheck = false;
     this.alertService.clear();
-    let message="Invalid User Name !";
     console.log("user name : password"+this.model.currentusername+this.model.currentpassword);
-    localStorage.setItem('currentusername',this.model.currentusername);
-    localStorage.setItem('currentpassword',this.model.currentpassword);
+    this.authenticationService.login(this.model.currentusername, this.model.currentpassword)
+      .subscribe(
+      data => {
+        this.user=data;
+        console.log("Status ====>", this.user.status);
+        if(this.user.status=="success") {                    
+          localStorage.setItem('currentusername',this.model.currentusername);
+          localStorage.setItem('currentpassword',this.model.currentpassword);
+          this.router.navigate(['/landingpage']);
+        } 
+        else {
+          this.loading = false;
+          this.errorStatus = this.user.status;
+          this.alertService.error(this.user.status);
+        }
 
-    if(this.model.currentusername!=="admin"){
-     // this.alertService.info(message);
-     // this.alertService.warn(message);
-     // this.alertService.success(message);
+      },
+      error => {
+          this.loading = false;
+      });
+          
+    /*if(this.model.currentusername!=="admin"){
       this.alertService.error(message);
-     // this.alertService.success(message);
-
     }
     else {
       this.router.navigate(['/landingpage']);
-
-    }
+    }*/
 
   }
 
   forgetPassword(){
+    this.SignIndiv = false;
+    this.usercheckdiv = true;
+    this.passwordcheck = false;
+    this.model.currentusername='';
+    this.model.currentpassword='';
+  }
 
+  checkUserName(){
+    localStorage.setItem('forgetUser',this.model.currentusername);
+    this.authenticationService.checkUserName(this.model.currentusername)
+    .subscribe(
+        data => {
+            this.user=data;
+            if(this.user.status=='success') {
+              this.usererrorStatus = "Valid UserName";
+              this.SignIndiv = false;
+              this.usercheckdiv = false;
+              this.passwordcheck = true;
+              this.model.currentusername='';
+            }else {
+              this.usererrorStatus = this.user.status;
+              this.SignIndiv = false;
+              this.usercheckdiv = true;
+              this.passwordcheck = false;
+            }
+
+        }, 
+        error => {
+          this.usererrorStatus = "NetWork Error";
+          this.SignIndiv = false;
+          this.usercheckdiv = true;
+          this.passwordcheck = false;
+        });
+  }
+
+  submitPassword(){
+    if(this.model.newPassword1 == this.model.newPassword2) {
+         this.authenticationService.resetPassword(this.model.newPassword1,localStorage.getItem('forgetUser'))
+         .subscribe(
+         data => {
+             this.user=data;
+             if(this.user.status=="success") {
+                this.SignIndiv = true;
+                this.usercheckdiv = false;
+                this.passwordcheck = false;
+                this.model.currentusername='';
+                this.model.currentpassword='';
+             }
+             else if(this.user.status == "failure"){
+                this.passworderror = "Password Not Match";
+                this.SignIndiv = false;
+                this.usercheckdiv = false;
+                this.passwordcheck = true;
+             }
+         },
+         error => {
+          this.passworderror = "Network Error";
+         });
+      }
+      else {
+          this.passworderror = "Password doesn't Matched.please try again";
+      }
   }
 
   showPassword(){
+    this.SignIndiv = true;
+    this.usercheckdiv = false;
+    this.passwordcheck = false;
     if (this.passwordtype=="text"){
       this.passwordtype="password";
-
     }
     if (this.passwordtype=="password"){
       this.passwordtype="text";
-
     }
    // var x = document.getElementById("myInput");
   //if (x.type === "password") {
