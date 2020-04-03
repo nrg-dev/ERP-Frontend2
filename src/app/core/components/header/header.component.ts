a9be373385a37740741cd16773fcc341fd783cf8
Mohamed Arif <duser3@neotural.com>
2020-04-03 11:02:03 +0530

    purchase page table issue fixed

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { InteractionService } from './../../common/_services/interaction.service';
 
 @Component({
