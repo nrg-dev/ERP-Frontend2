aff68be57d87afc8588b65d10e9de243db4326e4
nrg admin <46997932+alexjeff@users.noreply.github.com>
2020-04-05 17:43:35 +0800

    Purchase return component check in

--- src/app/templates/modules/purchase/purchase-routing.module.ts
+++ src/app/templates/modules/purchase/purchase-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PurchaseComponent } from './components/purchase/purchase.component';
 import { PurchaseInvoiceComponent } from './components/purchase-invoice/purchase-invoice.component';
+import { PurchaseReturnComponent } from './components/purchase-return/purchase-return.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'invoice',
     component: PurchaseInvoiceComponent
+},
+{
+  path: 'return',
+  component: PurchaseReturnComponent
 }
 ];
 
