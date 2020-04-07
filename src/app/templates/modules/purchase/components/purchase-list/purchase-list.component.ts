36035ceb5f1ebe6dcd085b39097384f106ff51d6
Mohamed Arif <duser3@neotural.com>
2020-04-07 18:16:43 +0530

    create invoice static done

--- src/app/templates/modules/purchase/components/purchase-list/purchase-list.component.ts
+++ src/app/templates/modules/purchase/components/purchase-list/purchase-list.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
-import { PurchaseService } from '../../services/purchase.service';
+import { Component, OnInit, OnDestroy, AfterViewInit } from "@angular/core";
+import { PurchaseService } from "../../services/purchase.service";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { MatDialog, MatDialogConfig, MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
+import {
+  MatDialog,
+  MatDialogConfig,
+  MatPaginator,
+  MatSort,
+  MatTableDataSource,
+} from "@angular/material";
 import { PurchaseAddComponent } from "../purchaseadd/purchaseadd.component";
-import { PurchaseCreateInvoiceComponent } from './../purchase-create-invoice/purchase-create-invoice.component';
+import { PurchaseCreateInvoiceComponent } from "./../purchase-create-invoice/purchase-create-invoice.component";
 
 @Component({
-  selector: 'app-purchaselist',
-  templateUrl: './purchase-list.component.html',
-  styleUrls: ['./purchase-list.component.scss', './purchase-list.component.css']
+  selector: "app-purchaselist",
+  templateUrl: "./purchase-list.component.html",
+  styleUrls: [
+    "./purchase-list.component.scss",
+    "./purchase-list.component.css",
+  ],
 })
-export class PurchaseListComponent  implements OnInit, OnDestroy  {
-
+export class PurchaseListComponent implements OnInit, OnDestroy {
   purchaseOrderList: any;
   dialogConfig = new MatDialogConfig();
-  prodArr     =  [];
-  isCheckedArr     =  [];
-  vendorArr     =  [];
+  prodArr = [];
+  isCheckedArr = [];
+  vendorArr = [];
   isCreateReturn: boolean = false;
   isDeleteButton: boolean = false;
   isCreateInvoice: boolean = false;
@@ -27,104 +35,112 @@ export class PurchaseListComponent  implements OnInit, OnDestroy  {
   isSortStatusAsc: boolean = true;
   isSortDateDesc: boolean = false;
   isSortDateAsc: boolean = true;
-  title: string = '';
-  button: string = '';
-  
-  constructor( 
-    private purchaseService:PurchaseService,
+  title: string = "";
+  button: string = "";
+
+  constructor(
+    private purchaseService: PurchaseService,
     private snackBar: MatSnackBar,
     private dialog: MatDialog
-    ) { 
-  }
+  ) {}
 
   ngOnInit() {
     this.getPurchaseOrderLists();
     this.removeScrollBar();
+    this.createInvoice();
   }
-  
-  ngOnDestroy(){
-    (<HTMLElement>document.querySelector('.mat-drawer-content')).style.overflow = 'auto';
+
+  ngOnDestroy() {
+    (<HTMLElement>(
+      document.querySelector(".mat-drawer-content")
+    )).style.overflow = "auto";
   }
 
   removeScrollBar() {
     setTimeout(function () {
-        (<HTMLElement>document.querySelector('.mat-drawer-content')).style.overflow = 'inherit';
-      }, 300);
+      (<HTMLElement>(
+        document.querySelector(".mat-drawer-content")
+      )).style.overflow = "inherit";
+    }, 300);
   }
   getDeleteButtonStyle() {
     if (!this.isDeleteButton) {
       let myStyles = {
-        'color': 'gray',
-        'background': '#1A2D39',
-        'border':'1px solid #1A2D39',
-        'display': 'none'
+        color: "gray",
+        background: "#1A2D39",
+        border: "1px solid #1A2D39",
+        display: "none",
       };
-    return myStyles;
+      return myStyles;
+    }
   }
-}  
 
-getCreateReturnStyle() {
-  if (!this.isCreateReturn) {
-    let myStyles = {
-      'color': 'gray',
-      'background': '#1A2D39',
-      'border':'1px solid #1A2D39',
-      'display': 'none'
-    };
-  return myStyles;
-}
-}   
+  getCreateReturnStyle() {
+    if (!this.isCreateReturn) {
+      let myStyles = {
+        color: "gray",
+        background: "#1A2D39",
+        border: "1px solid #1A2D39",
+        display: "none",
+      };
+      return myStyles;
+    }
+  }
 
-getCreateInvoiceStyle() {
-  if (!this.isCreateInvoice) {
-    let myStyles = {
-      'color': 'gray',
-      'background': '#1A2D39',
-      'border':'1px solid #1A2D39',
-      'display': 'none'
-    };
-  return myStyles;
-}
-}  
+  getCreateInvoiceStyle() {
+    if (!this.isCreateInvoice) {
+      let myStyles = {
+        color: "gray",
+        background: "#1A2D39",
+        border: "1px solid #1A2D39",
+        display: "none",
+      };
+      return myStyles;
+    }
+  }
 
-getAddPurchaseOrderStyle() {
-  if (this.isAddPurchaseOrder) {
-    let myStyles = {
-      'color': 'gray',
-      'background': '#1A2D39',
-      'border':'1px solid #1A2D39',
-      'display': 'none'
-    };
-  return myStyles;
-}
-}  
-  getPurchaseOrderLists(){
-    this.purchaseService.getPurchaseOrderLists()
-    .subscribe((res:[]) => { 
-      this.purchaseOrderList = res;
+  getAddPurchaseOrderStyle() {
+    if (this.isAddPurchaseOrder) {
+      let myStyles = {
+        color: "gray",
+        background: "#1A2D39",
+        border: "1px solid #1A2D39",
+        display: "none",
+      };
+      return myStyles;
+    }
+  }
+  getPurchaseOrderLists() {
+    this.purchaseService.getPurchaseOrderLists().subscribe(
+      (res: []) => {
+        this.purchaseOrderList = res;
       },
-      error => {
+      (error) => {
         setTimeout(() => {
-          this.snackBar.open("Network error: server is temporarily unavailable", "dismss", {
-            panelClass: ["error"],
-            verticalPosition: 'top'      
-          });
+          this.snackBar.open(
+            "Network error: server is temporarily unavailable",
+            "dismss",
+            {
+              panelClass: ["error"],
+              verticalPosition: "top",
+            }
+          );
         });
       }
     );
   }
-  
+
   rowSelected(index: number, item: any, isChecked: boolean) {
     if (isChecked) {
       item.indexVal = index;
       this.prodArr.push(item);
-      this.isCheckedArr.push({checked: true, indexVal:index});
-      this.vendorArr.push({vendorName: item.vendorname, indexVal:index});
+      this.isCheckedArr.push({ checked: true, indexVal: index });
+      this.vendorArr.push({ vendorName: item.vendorname, indexVal: index });
       this.isShowEditDelete[index] = false;
     } else {
-      this.removeItem(this.isCheckedArr, index, 'checked');
-      this.removeItem(this.prodArr, index, 'product');
-      this.removeItem(this.vendorArr, index, 'vendor');
+      this.removeItem(this.isCheckedArr, index, "checked");
+      this.removeItem(this.prodArr, index, "product");
+      this.removeItem(this.vendorArr, index, "vendor");
     }
 
     if (this.prodArr.length > 0) {
@@ -134,8 +150,8 @@ getAddPurchaseOrderStyle() {
         const vendorName = item.vendorname;
         if (this.prodArr.length > 1) {
           this.isCreateReturn = false;
-          if (status !== 'Invoiced') {
-            let getVendorName = '';
+          if (status !== "Invoiced") {
+            let getVendorName = "";
             this.vendorArr.forEach((item, indexCheck) => {
               if (indexCheck > 0) {
                 getVendorName = this.vendorArr[indexCheck - 1].vendorName;
@@ -150,7 +166,7 @@ getAddPurchaseOrderStyle() {
                   this.getErrorMsg(false);
                 }
               }
-             });
+            });
           } else {
             this.isDeleteButton = false;
             this.isCreateInvoice = false;
@@ -158,57 +174,57 @@ getAddPurchaseOrderStyle() {
         } else {
           this.isDeleteButton = false;
           this.getErrorMsg(false);
-          if (status === 'Open' && this.isCheckedArr[0].checked) {
+          if (status === "Open" && this.isCheckedArr[0].checked) {
             this.isCreateInvoice = true;
           } else {
             this.isCreateInvoice = false;
           }
-          if (status === 'Invoiced' && this.isCheckedArr[0].checked) {
+          if (status === "Invoiced" && this.isCheckedArr[0].checked) {
             this.isCreateReturn = true;
           } else {
             this.isCreateReturn = false;
           }
         }
-    });
-  } else {
-    this.isCreateInvoice = false;
-    this.isCreateReturn = false;
-    this.isDeleteButton = false;
-    this.isAddPurchaseOrder = false; 
+      });
+    } else {
+      this.isCreateInvoice = false;
+      this.isCreateReturn = false;
+      this.isDeleteButton = false;
+      this.isAddPurchaseOrder = false;
+    }
   }
-
-}
 
   addPurchaseOrder(id: string, item: any) {
     let data: any;
     if (id !== null) {
-      this.title = 'Edit Purchase Order';
-      this.button = 'Update';
-      item.dialogTitle  = this.title;
-      item.dialogText  = this.button;
-      item.selected     = false;
-      item.selected     = true;
-      data  = item;
+      this.title = "Edit Purchase Order";
+      this.button = "Update";
+      item.dialogTitle = this.title;
+      item.dialogText = this.button;
+      item.selected = false;
+      item.selected = true;
+      data = item;
     } else {
-      this.title = 'Add Purchase Order';
-      this.button = 'Add';
-      data        = {dialogTitle: this.title, dialogText: this.button};
+      this.title = "Add Purchase Order";
+      this.button = "Add";
+      data = { dialogTitle: this.title, dialogText: this.button };
     }
-    
+
     this.dialogConfig.disableClose = true;
     this.dialogConfig.autoFocus = true;
     this.dialogConfig.position = {
-      'top': '1000',
-      left: '100'
+      top: "1000",
+      left: "100",
     };
-    this.dialog.open(PurchaseAddComponent,{
-      panelClass: 'addpromotion',
-      data: data,
-    })
-    .afterClosed().subscribe(result => {
-      this.getPurchaseOrderLists ();
-    });
-    
+    this.dialog
+      .open(PurchaseAddComponent, {
+        panelClass: "addpromotion",
+        data: data,
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        this.getPurchaseOrderLists();
+      });
   }
 
   getErrorMsg(isErrMsg: boolean) {
@@ -217,11 +233,11 @@ getAddPurchaseOrderStyle() {
       setTimeout(() => {
         this.snackBar.open("Select only one vendor", "dismss", {
           panelClass: ["warn"],
-          verticalPosition: 'top'      
+          verticalPosition: "top",
         });
       });
     } else {
-      return '';
+      return "";
     }
   }
 
@@ -236,84 +252,83 @@ getAddPurchaseOrderStyle() {
       }
     });
 
-    if (type === 'checked') {
+    if (type === "checked") {
       this.isCheckedArr = isCheckedArr;
-    } else if (type === 'product') {
+    } else if (type === "product") {
       this.prodArr = isCheckedArr;
     } else {
       this.vendorArr = isCheckedArr;
     }
-    
   }
 
   sortByOrder(column: string, order: string) {
-      if (column === 'status' && order === 'desc') {
-        this.isSortStatusDesc = true;
-        this.isSortStatusAsc  = false;  
-        this.purchaseOrderList.sort((a,b)=>b.status.localeCompare(a.status));
-      } else if (column === 'status' && order === 'asc') {
-        this.isSortStatusDesc = false;
-        this.isSortStatusAsc  = true;  
-        this.purchaseOrderList.sort((a,b)=>a.status.localeCompare(b.status));
-      }
-      else if (column === 'date' && order === 'desc') {
-        this.isSortDateDesc = true;
-        this.isSortDateAsc  = false;  
-        this.purchaseOrderList.sort((a,b)=>b.date.localeCompare(a.date));
-      } else {
-        this.isSortDateDesc = false;
-        this.isSortDateAsc  = true;  
-        this.purchaseOrderList.sort((a,b)=>a.date.localeCompare(b.date));
-      }
+    if (column === "status" && order === "desc") {
+      this.isSortStatusDesc = true;
+      this.isSortStatusAsc = false;
+      this.purchaseOrderList.sort((a, b) => b.status.localeCompare(a.status));
+    } else if (column === "status" && order === "asc") {
+      this.isSortStatusDesc = false;
+      this.isSortStatusAsc = true;
+      this.purchaseOrderList.sort((a, b) => a.status.localeCompare(b.status));
+    } else if (column === "date" && order === "desc") {
+      this.isSortDateDesc = true;
+      this.isSortDateAsc = false;
+      this.purchaseOrderList.sort((a, b) => b.date.localeCompare(a.date));
+    } else {
+      this.isSortDateDesc = false;
+      this.isSortDateAsc = true;
+      this.purchaseOrderList.sort((a, b) => a.date.localeCompare(b.date));
+    }
   }
 
   removePurchaseOrder(id: string) {
-      this.purchaseService.removePurchaseOrder(id).subscribe((data: any) =>{
-        if (data === null) {
-          setTimeout(() => {
-            this.snackBar.open("Purchase order has been deleted successfully", "dismss", {
+    this.purchaseService.removePurchaseOrder(id).subscribe((data: any) => {
+      if (data === null) {
+        setTimeout(() => {
+          this.snackBar.open(
+            "Purchase order has been deleted successfully",
+            "dismss",
+            {
               panelClass: ["success"],
-              verticalPosition: 'top'      
-            });
+              verticalPosition: "top",
+            }
+          );
+        });
+        this.getPurchaseOrderLists();
+      } else if (data === 500) {
+        setTimeout(() => {
+          this.snackBar.open("Internal server error", "dismss", {
+            panelClass: ["error"],
+            verticalPosition: "top",
           });
-          this.getPurchaseOrderLists();
-        } else if (data === 500) {
-          setTimeout(() => {
-            this.snackBar.open("Internal server error", "dismss", {
-              panelClass: ["error"],
-              verticalPosition: 'top'      
-            });
+        });
+      } else {
+        setTimeout(() => {
+          this.snackBar.open("Bad request data", "dismss", {
+            panelClass: ["error"],
+            verticalPosition: "top",
           });
-        } else {
-          setTimeout(() => {
-            this.snackBar.open("Bad request data", "dismss", {
-              panelClass: ["error"],
-              verticalPosition: 'top'      
-            });
-          });
-        }
-      });
-      
+        });
+      }
+    });
   }
-  
-  createInvoice() {
 
+  createInvoice() {
     let data: any;
 
-    data = { dialogTitle: 'Add Purchase Order', dialogText: 'Add' };
+    data = { dialogTitle: "Add Purchase Order", dialogText: "Add" };
 
     this.dialogConfig.disableClose = true;
     this.dialogConfig.autoFocus = true;
     this.dialogConfig.position = {
-      'top': '1000',
-      left: '100'
+      top: "1000",
+      left: "100",
     };
 
     this.dialog.open(PurchaseCreateInvoiceComponent, {
-      panelClass: 'addpromotion',
+      panelClass: "purchaseCreateInvoice",
       data: data,
-    })
-    console.log('i am here', this.prodArr);
+    });
+    console.log("i am here", this.prodArr);
   }
-
 }
