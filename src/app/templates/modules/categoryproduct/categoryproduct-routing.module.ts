0c38e08e39da602e994134a6a619d171c293600c
Mohamed Arif <duser3@neotural.com>
2020-04-19 19:40:47 +0530

    router added

--- src/app/templates/modules/categoryproduct/categoryproduct-routing.module.ts
+++ src/app/templates/modules/categoryproduct/categoryproduct-routing.module.ts
@@ -2,25 +2,38 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router"; 
 
 // Components
-import { CategoryItemComponent, AddpromotionComponent, DiscounteditComponent, DiscountdeleteComponent, CategoryeditdeleteComponent, AddnewcategoryComponent, AddnewproductComponent ,ProductviewComponent, ProducteditComponent, AllproducteditComponent, CategorytableComponent} from './categoryitem/categoryitem.component';
+import { 
+  CategoryItemComponent, 
+  AddpromotionComponent, 
+  DiscounteditComponent, 
+  DiscountdeleteComponent, 
+  CategoryeditdeleteComponent, 
+  AddnewcategoryComponent, 
+  AddnewproductComponent ,
+  ProductviewComponent, 
+  ProducteditComponent, 
+  AllproducteditComponent, 
+  CategorytableComponent} from './categoryitem/categoryitem.component';
 import { UnitsComponent } from './units/units.component';
 import { AddunitsComponent } from './addunits/addunits.component';
 
 const routes: Routes = [
   {
       path: '',
-      component: CategoryItemComponent,
-      pathMatch: 'full'
+      pathMatch: 'full',
+     component: CategoryItemComponent,
   },
+
   {
     path: 'units',
     component: UnitsComponent,
     pathMatch: 'full'
-},
-{
-  path: '/category-and-product/addunits',
-  component: AddunitsComponent,
-}
+  },
+  {
+    path: 'addunits',
+    component: AddunitsComponent,
+  }
+
 ];
 
 @NgModule({
