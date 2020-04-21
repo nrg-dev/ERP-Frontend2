80a8e4171185ff1f5458c83e47e09fec2a791408
josni <shahaysjoseny@gmail.com>
2020-04-21 17:34:20 +0530

    Load UnitList and Back Icon change in Unit submenu

--- src/app/templates/modules/categoryproduct/units/units.component.ts
+++ src/app/templates/modules/categoryproduct/units/units.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CategoryproductService } from '../services/categoryproduct.service';
+import { MatSnackBar } from "@angular/material";
 
 @Component({
   selector: 'app-units',
@@ -7,31 +9,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./units.component.scss']
 })
 export class UnitsComponent implements OnInit {
+  constructor(private router: Router,
+    private catprodservice: CategoryproductService,
+    private snackBar: MatSnackBar
+  ) { }
 
-  constructor(private router: Router) { }
-
-  public NAMES = [];
-  unitlist:any;
+  unitlist:any = {};
 
   ngOnInit() {
    
-for (let i = 1; i < 100; i++) {
-    let newName = {
-       id:i.toString(),
-       value1:"Ubalton",
-       value2:"+91 88704662431",
-       value3:"alex@gmail.com",
-       value4:"Tamil Nadu",
-       value5:"India",
-       value6:"+91 4763212",
-       value7:"10000",
-       value8:"MCA",
-       value9:"Male",
-       value10:"Active",
-       value11:"Delhi",
-    };
-    this.NAMES.push(newName);
-}
+    let id = "all";
+     //--load unitList
+     this.catprodservice.loadUnitList(id)
+      .subscribe(
+        data => {
+          this.unitlist = data;
+        },
+        error => {
+          setTimeout(() => {
+            this.snackBar.open("Network error: server is temporarily unavailable", "dismss", {
+              panelClass: ["error"],
+              verticalPosition: 'top'      
+            });
+          });   
+        }
+      );
+
   }
   addNewUnits(){
     this.router.navigate(["category-and-product/addunits"]);
