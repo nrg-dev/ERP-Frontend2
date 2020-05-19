e8e46648cf504c326c05d344df81491cb088d848
josni <shahaysjoseny@gmail.com>
2020-05-19 19:27:49 +0530

    radio button change on sales

--- src/app/templates/modules/purchase/components/purchase-create-return/purchase-create-return.component.ts
+++ src/app/templates/modules/purchase/components/purchase-create-return/purchase-create-return.component.ts
@@ -17,9 +17,6 @@ export class PurchaseCreateReturnComponent implements OnInit {
   returnType:string;
   quantity:number;
 
-  public itemstatuserror = false;
-  public paymentTypeerrors = false;
-
   constructor(    
     public dialogRef: MatDialogRef<PurchaseCreateReturnComponent>,
     @Inject(MAT_DIALOG_DATA) public data, 
@@ -35,8 +32,6 @@ export class PurchaseCreateReturnComponent implements OnInit {
     }
 
   ngOnInit() {
-    this.itemstatuserror = false;
-    this.paymentTypeerrors = false;
   }
 
   getPrice(quantity:number){
@@ -50,7 +45,7 @@ export class PurchaseCreateReturnComponent implements OnInit {
             panelClass: ["warning"],
             verticalPosition: "top",
           }
-        );
+        );   
       });
     }
     if(price == Infinity){
@@ -66,34 +61,49 @@ export class PurchaseCreateReturnComponent implements OnInit {
 
   getPaymentValid(paymentType:string){
     if(paymentType == null){
-      this.paymentTypeerrors = true;
-      document.getElementById("paymentType").style.background = '#c18484';
+      document.getElementById("cash").style.background = '#c18484';
+      document.getElementById("credit").style.background = '#c18484';
+      document.getElementById("voucher").style.background = '#c18484';
     }else{
-      this.paymentTypeerrors = false;
-      document.getElementById("paymentType").style.background = '#1A2D39';
+      document.getElementById("cash").style.background = '#1A2D39';
+      document.getElementById("credit").style.background = '#1A2D39';
+      document.getElementById("voucher").style.background = '#1A2D39';
     }
   }
 
   getItemValid(itemstatus:string){
     if(itemstatus == null){
-      this.itemstatuserror = true;
-      document.getElementById("itemstatus").style.background = '#c18484';
+      document.getElementById("damaged").style.background = '#c18484';
+      document.getElementById("expired").style.background = '#c18484';
+      document.getElementById("others").style.background = '#c18484';
     }else{
-      this.itemstatuserror = false;
-      document.getElementById("itemstatus").style.background = '#1A2D39';
+      document.getElementById("damaged").style.background = '#1A2D39';
+      document.getElementById("expired").style.background = '#1A2D39';
+      document.getElementById("others").style.background = '#1A2D39';
     }
   }
 
   addReturn() {
     if(this.model.itemstatus == null){
       console.log("ItemStatus not chosen");
-      this.itemstatuserror = true;
-      document.getElementById("itemstatus").style.background = '#c18484';
+      document.getElementById("damaged").style.background = '#c18484';
+      document.getElementById("expired").style.background = '#c18484';
+      document.getElementById("others").style.background = '#c18484';
     }
     if(this.model.paymentType == null){
       console.log("Payment not chosen");
-      this.paymentTypeerrors = true;
-      document.getElementById("paymentType").style.background = '#c18484';
+      document.getElementById("cash").style.background = '#c18484';
+      document.getElementById("credit").style.background = '#c18484';
+      document.getElementById("voucher").style.background = '#c18484';
+    }else if(this.model.quantity > this.model.invqty){
+      this.snackBar.open(
+        "Qty cannot be more than Invoiced Qty.",
+        "dismss",
+        {
+          panelClass: ["warning"],
+          verticalPosition: "top",
+        }
+      );
     }else{
       const invoice = {
         "createddate": new Date().toJSON().slice(0, 10).split('-').reverse().join('/'),
--- src/app/templates/modules/sales/components/sales-create-return/sales-create-return.component.ts
+++ src/app/templates/modules/sales/components/sales-create-return/sales-create-return.component.ts
@@ -59,49 +59,96 @@ export class SalesCreateReturnComponent implements OnInit {
   close() {
     this.dialogRef.close();
   }
-  addReturn() {
-    const invoice = {
-      "createddate": new Date().toJSON().slice(0, 10).split('-').reverse().join('/'),
-      "invoicedqty": this.model.invqty,
-      "customercode" : this.model.customercode,
-      "customername" : this.model.customername,
-      "itemname" : this.model.productname,
-      "itemStatus" : this.model.itemstatus,
-      "returnStatus" : this.model.paymentType,
-      "qty" : this.model.quantity,
-      "invoiceddate" : this.model.date,
-      "price" : this.model.price
+
+  getPaymentValid(paymentType:string){
+    if(paymentType == null){
+      document.getElementById("cash").style.background = '#c18484';
+      document.getElementById("credit").style.background = '#c18484';
+      document.getElementById("voucher").style.background = '#c18484';
+    }else{
+      document.getElementById("cash").style.background = '#1A2D39';
+      document.getElementById("credit").style.background = '#1A2D39';
+      document.getElementById("voucher").style.background = '#1A2D39';
     }
-    this.salesService.createReturn(invoice).subscribe(
-      (respose) => {
-        if (respose === null) {
+  }
+
+  getItemValid(itemstatus:string){
+    if(itemstatus == null){
+      document.getElementById("damaged").style.background = '#c18484';
+      document.getElementById("expired").style.background = '#c18484';
+      document.getElementById("others").style.background = '#c18484';
+    }else{
+      document.getElementById("damaged").style.background = '#1A2D39';
+      document.getElementById("expired").style.background = '#1A2D39';
+      document.getElementById("others").style.background = '#1A2D39';
+    }
+  }
+
+  addReturn() {
+    if(this.model.itemstatus == null){
+      console.log("ItemStatus not chosen");
+      document.getElementById("damaged").style.background = '#c18484';
+      document.getElementById("expired").style.background = '#c18484';
+      document.getElementById("others").style.background = '#c18484';
+    }
+    if(this.model.paymentType == null){
+      console.log("Payment not chosen");
+      document.getElementById("cash").style.background = '#c18484';
+      document.getElementById("credit").style.background = '#c18484';
+      document.getElementById("voucher").style.background = '#c18484';
+    }else if(this.model.quantity > this.model.invqty){
+      this.snackBar.open(
+        "Qty cannot be more than Invoiced Qty.",
+        "dismss",
+        {
+          panelClass: ["warning"],
+          verticalPosition: "top",
+        }
+      );
+    }else{
+      const invoice = {
+        "createddate": new Date().toJSON().slice(0, 10).split('-').reverse().join('/'),
+        "invoicedqty": this.model.invqty,
+        "customercode" : this.model.customercode,
+        "customername" : this.model.customername,
+        "itemname" : this.model.productname,
+        "itemStatus" : this.model.itemstatus,
+        "returnStatus" : this.model.paymentType,
+        "qty" : this.model.quantity,
+        "invoiceddate" : this.model.date,
+        "price" : this.model.price
+      }
+      this.salesService.createReturn(invoice).subscribe(
+        (respose) => {
+          if (respose === null) {
+            setTimeout(() => {
+              this.snackBar.open(
+                "Sales Return Created Successfully",
+                "dismss",
+                {
+                  panelClass: ["success"],
+                  verticalPosition: "top",
+                }
+              );
+
+            });
+            this.close();
+          }
+        },
+        (error) => {
           setTimeout(() => {
             this.snackBar.open(
-              "Sales Return Created Successfully",
+              "Network error: server is temporarily unavailable",
               "dismss",
               {
-                panelClass: ["success"],
+                panelClass: ["error"],
                 verticalPosition: "top",
               }
             );
-
           });
-          this.close();
         }
-      },
-      (error) => {
-        setTimeout(() => {
-          this.snackBar.open(
-            "Network error: server is temporarily unavailable",
-            "dismss",
-            {
-              panelClass: ["error"],
-              verticalPosition: "top",
-            }
-          );
-        });
-      }
-    );
+      );
+    }
   }
 
 }
